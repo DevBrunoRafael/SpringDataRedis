12d305d34b9305a0630f75e0f5f5f7f20e59f893
Bruno Rafael <brunorafaeldls2016@gmail.com>
2022-10-28 18:43:19 -0300

    redis enable

--- src/main/java/com/devbrunorafael/springdataredis/SpringDataRedisApplication.java
+++ src/main/java/com/devbrunorafael/springdataredis/SpringDataRedisApplication.java
@@ -2,8 +2,10 @@ package com.devbrunorafael.springdataredis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 @SpringBootApplication
+@EnableRedisRepositories
 public class SpringDataRedisApplication {
 
     public static void main(String[] args) {
